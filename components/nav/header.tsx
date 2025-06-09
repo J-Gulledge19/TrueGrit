4a852ca25626fd9b1590c80497ef70a755116778
Josh Gulledge <jgulledge19@gmail.com>
2025-06-09 12:19:23 -0500

    Added h1 and removed extra styling code from header image after image got darkened

--- components/nav/header.tsx
+++ components/nav/header.tsx
@@ -3,15 +3,16 @@ import NavBar from "./navbar";
 export default function Header () {
     return (
         <header className="relative h-30 w-full shadow-2xl">
-            <div className="absolute flex justify-between inset-0 bg-[url(/images/portfolio/headerimg.jpg)] bg-cover bg-center before:content-[''] before:absolute before:inset-0 before:bg-black before:opacity-50 z-0"></div>
+            <div className="absolute flex justify-between inset-0 bg-[url(/images/portfolio/headerimg.jpg)] z-0"></div>
             <img src="/images/portfolio/wire.png" className="absolute  w-screen h-52 opacity-30"></img>
             <section className="relative flex justify-between z-10 p-1">
                 <section className=" flex flex-col justify-around p-4 w-full">
                     <h1 className="ml-10 mt-2 text-6xl font-western text-grit-parchment uppercase">True Grit Welding</h1>
+                    <h1 className="ml-10 text-3xl font-western text-grit-parchment">Built with Grit. Forged with Precision.</h1>
                     <p className="ml-10 text-lg text-grit-steel">Certified & Insured – Repair & Custom Fabrication</p>
                     <NavBar />
                 </section>
-                <img className="w-52" src="/images/portfolio/TrueGrit.png" alt="logo" />
+                    <img className="w-48" src="/images/portfolio/TrueGrit.png" alt="logo" />
             </section>
         </header>
     )
