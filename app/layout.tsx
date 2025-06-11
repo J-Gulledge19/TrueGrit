0d69f5c2de726b00e767516519abe654ac9771e5
Josh Gulledge <jgulledge19@gmail.com>
2025-06-11 09:35:53 -0500

    Added icon in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css'
-import { Cinzel, Anton, Bebas_Neue } from 'next/font/google'
-
-import Header from '../components/nav/header'
-import Footer from '../components/footer'
+import '../styles/globals.css';
+import { Cinzel, Anton, Bebas_Neue } from 'next/font/google';
+// import Head from 'next/head';
+import Header from '../components/nav/header';
+import Footer from '../components/footer';
 
 const cinzel = Cinzel({ subsets: ['latin'], weight: '700' })
 const anton = Anton({ subsets: ['latin'], weight: '400' })
@@ -11,6 +11,9 @@ const bebas = Bebas_Neue({ subsets: ['latin'], weight: '400' })
 export const metadata = {
   title: 'True Grit Welding',
   description: 'Certified & Insured Welding - Repair & Custom Fabrication',
+  icons: {
+    icon: '/images/portfolio/TrueGrit.png',
+  }
 }
 
 export default function RootLayout({ children }) {
